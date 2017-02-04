7b6166ab834b61aa67846f8b55a3047b3a55062d
Matthew Bush <mbush@hirebotics.com>
2017-02-04 13:19:44 -0600

    Refactored presentation from business logic for the FAQ page

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,16 +1,29 @@
-import {React, Component} from 'react'
+import React from 'react'
 
-function puke(data){
+function puke (data){
     return <pre>{JSON.stringify(data, null, '')} </pre>
   }
   
-function render(props) {
-    console.log(props)
-    return props.isLoading == true 
-    ? <p> LOADING </p>
-    : <div>
-     {this.puke(props.faqs)}
-    </div>
+ function FAQ (props) {
+    return  (
+    props.isLoading === true
+    ?<div> LOADING </div>
+    : <div>{puke(props.faqs)} </div>
+    )
   }
 
-export default render
+module.exports = FAQ
+
+// var React = require('react');
+
+// function puke (obj) {
+//   return <pre>{JSON.stringify(obj, null, ' ')}</pre>
+// }
+
+// function ConfirmBattle (props) {
+//   return props.isLoading === true
+//     ? <p>LOADING</p>
+//     : <div>CONFIRM BATTLE: {puke(props)}</div>
+// }
+
+// module.exports = ConfirmBattle;
--- src/modules/FAQContainer.js
+++ src/modules/FAQContainer.js
@@ -1,53 +1,53 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 import firebase from 'firebase'
 import ReactFireMixin from 'reactfire'
-import {app} from '../components/firebaseApp'
+import { app } from '../components/firebaseApp'
 import FAQ from '../components/FAQ'
 
+console.log(FAQ)
 var ref = firebase.database().ref("/faqs")
 
 class FAQContainer extends Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {
-      faqs:[],
-      isLoading:true
+      faqs: [],
+      isLoading: true
     }
-  } 
-
-  puke(data){
-    return <pre>{JSON.stringify(data, null, '')} </pre>
   }
 
-  componentWillMount(){
+  componentWillMount() {
 
     this.setState(
       {
-        isLoading:true
+        isLoading: true
       }
     )
-  
+
   }
 
-  componentDidMount(){
-    ref.on('value', (data)=>{
-      console.log('[FIRE_BASE]',data.val())
+  componentDidMount() {
+    ref.on('value', (data) => {
+      console.log('[FIRE_BASE]', data.val())
       this.setState({
-        isLoading:false,
-         faqs:data.val()
+        isLoading: false,
+        faqs: data.val()
       })
-      
+
     })
   }
 
   render() {
-    console.log(this.state.faqs)
-    return this.state.isLoading == true 
-    ? <p> LOADING </p>
-    : <div>
-     {this.puke(this.state.faqs)}
+    return (
+      <div>
+    <FAQ
+      isLoading={this.state.isLoading}
+      faqs={this.state.faqs}
+    />
     </div>
+    );
+    
   }
 }
 
