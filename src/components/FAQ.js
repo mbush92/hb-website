47dcbb51876f5768b82b041afd6e88fd639db721
Matthew Bush <mbush@hirebotics.com>
2017-02-04 13:20:16 -0600

    cleaning up the file

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -13,17 +13,3 @@ function puke (data){
   }
 
 module.exports = FAQ
-
-// var React = require('react');
-
-// function puke (obj) {
-//   return <pre>{JSON.stringify(obj, null, ' ')}</pre>
-// }
-
-// function ConfirmBattle (props) {
-//   return props.isLoading === true
-//     ? <p>LOADING</p>
-//     : <div>CONFIRM BATTLE: {puke(props)}</div>
-// }
-
-// module.exports = ConfirmBattle;
